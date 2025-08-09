713cc390d1e4248aeb587ea580f8d08607b26c9b
GNaves <gabrielnaves60@gmail.com>
2025-08-09 14:48:46 -0300

    feat: adicionando novos componentes

---
+++ src/app/pages/certificados/certificados.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecondaryBottom } from "../../components/secondary-bottom/secondary-bottom";
+
+@Component({
+  selector: 'app-certificados',
+  imports: [SecondaryBottom],
+  templateUrl: './certificados.html',
+  styleUrl: './certificados.css'
+})
+export class Certificados {
+
+}
